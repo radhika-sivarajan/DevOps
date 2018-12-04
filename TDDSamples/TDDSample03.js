fbde084364f46c9103d1c7dd6e15caa8d93c0fe4
Radhika Sivarajan <radhikaraj333@gmail.com>
2018-12-04 14:14:58 +0000

    request npm

--- TDDSamples/TDDSample03.js
+++ TDDSamples/TDDSample03.js
@@ -8,12 +8,14 @@ for(var key in data){
 var request = require("request");
 
 // We then run the request module on a URL with a JSON
-request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&r=json", function(error, response, body) {
+request("http://www.omdbapi.com/?apikey=70b61d35&t=remember+the+titans", function(error, response, body) {
+    // console.log(body);
 
-  // If there were no errors and the response code was 200 (i.e. the request was successful)...
+//   If there were no errors and the response code was 200 (i.e. the request was successful)...
   if (!error && response.statusCode === 200) {
 
     // Then we print out the imdbRating
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+    // console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+    console.log("The movie'data: " + JSON.stringify(body));
   }
 });
