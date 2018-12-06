49d9f0508c8c9e52a55ffc2be36a372aa293e916
Radhika Sivarajan <radhikaraj333@gmail.com>
2018-12-06 23:48:54 +0000

    Route

--- React/heros-app/src/App.js
+++ React/heros-app/src/App.js
@@ -2,14 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import dcComics from './dc_comics.png';
-
-function home() {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
@@ -18,9 +11,25 @@ class App extends Component {
         <div className="column">
           <p>Welcome to the Heros application</p>
         </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+          </ul>
+        </nav>
         <div className="column">
           <img src={dcComics} alt="DC comics" />
         </div>
+        <Route path="/" exact component={Index} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} />
       </div>
     );
   }
