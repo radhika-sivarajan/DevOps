4c5a45800598e13a03f07374fda42f8561abe958
Radhika Sivarajan <radhikaraj333@gmail.com>
2018-12-11 23:18:53 +0000

    parsing

--- TDDSamples/TDDSample03.js
+++ TDDSamples/TDDSample03.js
@@ -1,4 +1,4 @@
-var iData = require("./TDDSampleExp");
+var iData = require("./TDDSample03Exp");
 var data = iData.myData;
 
 for (var key in data) {
@@ -8,17 +8,12 @@ for (var key in data) {
 var request = require("request");
 var movieName = process.argv[2];
 
-console.log(movieName);
 // We then run the request module on a URL with a JSON
 request("http://www.omdbapi.com/?apikey=70b61d35&t=" + movieName, function (error, response, body) {
-
   //   If there were no errors and the response code was 200 (i.e. the request was successful)...
   if (!error && response.statusCode === 200) {
-
     // Then we print out the data
-    console.log("The movie's details: "
-      + JSON.parse(body).Title + " ("
-      + JSON.parse(body).Year + ") Rating : "
-      + JSON.parse(body).imdbRating);
+    console.log("The details of movie " + JSON.parse(body).Title);
+    console.log(JSON.parse(body, null, 2));
   }
 });
---
+++ TDDSamples/TDDSample03Exp.js
@@ -0,0 +1,12 @@
+exports.myData = {
+    name: "radhika",
+    age: "28",
+    sex: "female"
+};
+
+var nonData = {
+    name: "dfcfcf",
+    age: "284",
+    sex: "male"
+};
+
