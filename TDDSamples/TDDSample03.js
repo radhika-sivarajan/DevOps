3f0f8838bf51159baeb7bc7e41347c214b5fb648
Radhika Sivarajan <radhikaraj333@gmail.com>
2018-12-11 22:52:20 +0000

    OMdb data request

--- TDDSamples/TDDSample03.js
+++ TDDSamples/TDDSample03.js
@@ -1,21 +1,24 @@
 var iData = require("./TDDSampleExp");
 var data = iData.myData;
 
-for(var key in data){
-    console.log(data[key]);
+for (var key in data) {
+  console.log(data[key]);
 }
 
 var request = require("request");
+var movieName = process.argv[2];
 
+console.log(movieName);
 // We then run the request module on a URL with a JSON
-request("http://www.omdbapi.com/?apikey=70b61d35&t=remember+the+titans", function(error, response, body) {
-    // console.log(body);
+request("http://www.omdbapi.com/?apikey=70b61d35&t=" + movieName, function (error, response, body) {
 
-//   If there were no errors and the response code was 200 (i.e. the request was successful)...
+  //   If there were no errors and the response code was 200 (i.e. the request was successful)...
   if (!error && response.statusCode === 200) {
 
-    // Then we print out the imdbRating
-    // console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-    console.log("The movie'data: " + JSON.stringify(body));
+    // Then we print out the data
+    console.log("The movie's details: "
+      + JSON.parse(body).Title + " ("
+      + JSON.parse(body).Year + ") Rating : "
+      + JSON.parse(body).imdbRating);
   }
 });
