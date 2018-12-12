6ae576950064167146914b30ea5d92114be7c310
Radhika Sivarajan <radhikaraj333@gmail.com>
2018-12-12 00:28:09 +0000

    Date TDD

---
+++ TDDSamples/strftime_test.js
@@ -0,0 +1,24 @@
+var date = new Date(2009, 9, 2);
+try {
+  assert("%Y should return full year",
+    date.strftime("%Y") === "2009");
+  assert("%m should return month",
+    date.strftime("%m") === "10");
+  assert("%d should return date",
+    date.strftime("%d") === "02");
+  assert("%y should return year as two digits",
+    date.strftime("%y") === "09");
+  assert("%F should act as %Y-%m-%d",
+    date.strftime("%F") === "2009-10-02");
+  console.log(assert.count + " tests OK");
+} catch (e) {
+  console.log("Test failed: " + e.message);
+}
+
+function assert(message, expr) {
+  if (!expr) {
+    throw new Error(message);
+  }
+  assert.count++;
+  return true;
+}
