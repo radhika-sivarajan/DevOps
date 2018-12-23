97285cb6187f163e437dabbe981a9432fd8b091b
Radhika Sivarajan <radhikaraj333@gmail.com>
2018-12-23 02:54:37 +0000

    Route files

---
+++ React/web-scrapping-MERN/server.js
@@ -0,0 +1,40 @@
+// Require our dependecies
+var express = require("express");
+var mongoose = require("mongoose");
+var bluebird = require("bluebird");
+var bodyParser = require("body-parser");
+var passport = require('passport');
+var session = require('express-session');
+
+var PORT = process.env.PORT || 4000;
+mongoose.Promise = bluebird;
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
+
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+var routes = require("./routes/routes");
+app.use("/", routes);
+
+// var mongoDB = "mongodb://heroku_qwcxn1pp:2218m91ki7ieu9vsnkcj35dhma@ds139879.mlab.com:39879/heroku_qwcxn1pp" ;
+
+var mongoDB = "mongodb://localhost/webScrapeApp";
+
+mongoose.connect(mongoDB, {
+  useMongoClient: true
+});
+
+//Get the default connection
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+// Start the server
+app.listen(PORT, function () {
+  console.log("Visit localhost:%s in your browser.", PORT);
+});
--- React/web-scrapping-MERN/webpack.config.js
+++ React/web-scrapping-MERN/webpack.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    entry: "./app/app.js",
-    output: {
-      filename: "public/bundle.js"
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?$/,
-          include: /app/,
-          loader: "babel",
-          query: {
-            presets: ["react", "es2015"]
-          }
+  entry: "./app/app.js",
+  output: {
+    filename: "public/bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: /app/,
+        loader: "babel",
+        query: {
+          presets: ["react", "es2015"]
         }
-      ]
-    },
-    devtool: "eval-source-map"
-  };
+      }
+    ]
+  },
+  devtool: "eval-source-map"
+};
