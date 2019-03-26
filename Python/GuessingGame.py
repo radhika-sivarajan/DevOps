81fb042521fee1b8ce2de18c424e3ba20f7377af
Radhika Sivarajan <radhikaraj333@gmail.com>
2019-03-26 22:05:38 +0000

    Guess game solution

--- Python/GuessingGame.py
+++ Python/GuessingGame.py
@@ -1,25 +1,11 @@
-###########################
-## PART 10: Simple Game ###
-### --- CODEBREAKER --- ###
-## --Nope--Close--Match--##
-###########################
-
-# It's time to actually make a simple command line game so put together everything
-# you've learned so far about Python. The game goes like this:
 
 # 1. The computer will think of 3 digit number that has no repeating digits.
 # 2. You will then guess a 3 digit number
 # 3. The computer will then give back clues, the possible clues are:
-#
 #     Close: You've guessed a correct number but in the wrong position
 #     Match: You've guessed a correct number in the correct position
 #     Nope: You haven't guess any of the numbers correctly
-#
-# 4. Based on these clues you will guess again until you break the code with a
-#    perfect match, the game will report "CODE CRACKED"!
-
-# There are a few things you will have to discover for yourself for this game!
-# Here are some useful hints:
+# 4. Based on these clues you will guess again until you break the code with a perfect match, the game will report "CODE CRACKED"!
 
 import random
 
@@ -27,7 +13,9 @@ def get_guess():
     '''
     Asks for the number guess and transforms the string to a list.
     '''
-    return list(input("What is your guess?"))
+    user_input = input("What is your guess?")
+    to_list = map(str, str(user_input))
+    return to_list
 
 def generate_code():
     '''
@@ -64,7 +52,6 @@ print("Welcome Code Breaker! Let's see if you can guess my 3 digit number!")
 # Create a Secret Code to start the Game
 secretCode = generate_code()
 print("Code has been generated, please guess a 3 digit number")
-#print(secretCode)
 
 # Empty Clue Report to Start with
 clueReport = []
@@ -76,7 +63,7 @@ while clueReport != "CODE CRACKED":
     guess = get_guess()
 
     # Give the clues
-    clueReport = generate_clues(guess,secretCode)
+    clueReport = generate_clues(secretCode,guess)
     print("Here is the result of your guess:")
     for clue in clueReport:
         print(clue)
