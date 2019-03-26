3b402bf52e938fe3400ad127bf78a1133d4118b8
Radhika Sivarajan <radhikaraj333@gmail.com>
2019-03-26 13:10:15 +0000

    Clue gen and game logic

--- Python/GuessingGame.py
+++ Python/GuessingGame.py
@@ -1,8 +1,10 @@
 import random
 
+
 # Get user guess
 def get_guess():
     return list(input("Enter a your guess, a 3 digit number! "))
+
 
 # Generate random 3 digit number (as list)
 def gen_number():
@@ -12,11 +14,31 @@ def gen_number():
     # Grab first 3
     return digits[:3]
 
+
 # Generate clues
-def gen_clues():
-    
+def gen_clues(user_guess, random_number):
+    if user_guess ==random_number:
+        return "Code cracked!!"
+
+    clues = []
+    for ind, num in enumerate(user_guess):
+        if num == random_number[ind]:
+            clues.append("Match")
+        elif num in code:
+            clues.append("Close")
+
+    if clues == []:
+        return "Nope"
+    else:
+        return clues
+
 
 # Game logic
-
-# print(get_guess())
-print(gen_number())
+print("Welcome to Gussing Game!!")
+secret_code = gen_number()
+clue_report = []
+while clue_report != "Code cracked!!":
+    guess = get_guess()
+    clue_report = gen_clues(guess, secret_code)
+    for clue in clue_report:
+        print(clue)
