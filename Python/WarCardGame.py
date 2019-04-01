78c90e39b89f5d54e8d267277851407474d4a7b7
Radhika Sivarajan <radhikaraj333@gmail.com>
2019-04-02 00:01:06 +0100

    Methods for game

--- Python/WarCardGame.py
+++ Python/WarCardGame.py
@@ -12,29 +12,76 @@ from random import shuffle
 SUITE = 'H D S C'.split()
 RANKS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
 
+
 class Deck:
     """
-    This is the Deck Class. This object will create a deck of cards to initiate
-    play. You can then use this Deck list of cards to split in half and give to
-    the players. It will use SUITE and RANKS to create the deck. It should also
-    have a method for splitting/cutting the deck in half and Shuffling the deck.
+    This is the Deck Class. This object will create a deck of cards to initiate play.
+    You can then use this Deck list of cards to split in half and give to the players.
+    It will use SUITE and RANKS to create the deck. It should also have a method for
+    splitting/cutting the deck in half and Shuffling the deck.
     """
-    pass
+    def __init__(self):
+        self.allcards = [(s,r) for s in SUITE for r in RANKS]  # Ordered deck
+
+    def shuffle(self):
+        shuffle(self.allcards) # Shuffle deck
+
+    def split_in_half(self):
+        return (self.allcards[:26],self.allcards[26:])
+
 
 class Hand:
     '''
     This is the Hand class. Each player has a Hand, and can add or remove
     cards from that hand. There should be an add and remove card method here.
     '''
-    pass
+    def __init__(self,cards):
+        self.cards = cards
+
+    def __str__(self):
+        return "Contain {} cards".format(len(self.cards))
+
+    def add_cards(self, addcards):
+        self.cards.extend(addcards)
+
+    def remove_cards(self):
+        self.cards.pop()
+
 
 class Player:
     """
     This is the Player class, which takes in a name and an instance of a Hand
     class object. The Payer can then play cards and check if they still have cards.
     """
-    pass
+    def  __init__(self, name, hand):
+        self.name = name
+        self.hand = hand
+
+    def play_card(self):
+        drawn_card = self.hand.remove_cards()
+        print("{} placed card {} /n".format(self.name, drawn_card))
+        return drawn_card
+
+    def remove_war_cards(self):
+        war_cards = []
+        if len(self.hand.cards) < 3:
+            return war_cards
+        else:
+            for x in range(3):
+                war_cards.append(self.hand.cards.pop(0))
+                # war_cards.append(self.hand.remove_cards())
+            return war_cards
+
+    def still_has_cards(self):
+        """
+        Returns True if player still has cards
+        """
+        return len(self.hand.cards) != 0
 
 
 # Play Game
-print("Welcome to War, let's begin...")
+print(Deck().split_in_half()[1])
+# Create New Deck and split in half
+d = Deck()
+d.shuffle()
+half1,half2 = d.split_in_half()
