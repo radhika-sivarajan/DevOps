b861e3aaa598ac9c39f4fd8eaac14960c5dd072e
Radhika Sivarajan <radhikaraj333@gmail.com>
2019-04-09 23:53:23 +0100

    dict comprehensions

--- Python/pythonStrings.py
+++ Python/pythonStrings.py
@@ -11,3 +11,15 @@ print(squared)
 
 
 squared = [x**2 for x in range(10)]
+
+
+mcase = {'a': 10, 'b': 34, 'A': 7, 'Z': 3}
+
+mcase_frequency = {
+    k.lower(): mcase.get(k.lower(), 0) + mcase.get(k.upper(), 0)
+    for k in mcase.keys()
+}
+
+# mcase_frequency == {'a': 17, 'z': 3, 'b': 34}
+
+{v: k for k, v in some_dict.items()}
