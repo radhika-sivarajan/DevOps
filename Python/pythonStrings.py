d50a327bf0c4bae8d58d972f795e65a4f904a59d
Radhika Sivarajan <radhikaraj333@gmail.com>
2019-04-12 23:44:37 +0100

    Reg expression

--- Python/pythonStrings.py
+++ Python/pythonStrings.py
@@ -1,25 +1,11 @@
-multiples_gen = (i for i in range(30) if i % 3 == 0)
-print(multiples_gen)
-# Output: <generator object <genexpr> at 0x7fdaa8e407d8>
-for x in multiples_gen:
-  print(x)
-  # Outputs numbers
+import re
 
-squared = {x**2 for x in [1, 1, 2]}
-print(squared)
-# Output: {1, 4}
+patterns = ["term1", "term2"]
+text = "This text have term1 not other"
 
-
-squared = [x**2 for x in range(10)]
-
-
-mcase = {'a': 10, 'b': 34, 'A': 7, 'Z': 3}
-
-mcase_frequency = {
-    k.lower(): mcase.get(k.lower(), 0) + mcase.get(k.upper(), 0)
-    for k in mcase.keys()
-}
-
-# mcase_frequency == {'a': 17, 'z': 3, 'b': 34}
-
-{v: k for k, v in some_dict.items()}
+for pattern in patterns:
+    print("The term is "+pattern)
+    if re.search(pattern, text):
+        print("Match")
+    else:
+        print("NO Match")
