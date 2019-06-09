430116d618986494189afe0584e7e105f466591e
Radhika Sivarajan <radhikaraj333@gmail.com>
2019-06-09 20:09:08 +0100

    JS solutions

--- Javascript/JS_Solutions/NumberProblems.js
+++ Javascript/JS_Solutions/NumberProblems.js
@@ -1,5 +1,3 @@
-var lineBreak = "\n-------------\n";
-
 // Write a javascript function named is_integer which checks if the passed argument is an integer. You can use any mathematical operator or functions defined in the Math object.
 function is_integer(n) {
     if (isNaN(n))
